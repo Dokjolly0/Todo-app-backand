79905a4aa1c96ea750283a9357513af707158a1b
Dokjolly0 <alexviolattolibero.it@gmail.com>
2025-01-28 17:01:53 +0100

    File upload require change

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 // Aumenta il limite di dimensione per le richieste POST a 10MB
-app.use(bodyParser.json({ limit: "10mb" })); // Puoi scegliere una dimensione maggiore se necessario
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/todoapp", apiRouter);
 app.use(errorHandlers);
